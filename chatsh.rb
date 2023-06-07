277de711b2b0cf7e18e3f1e67670112772206a91
h3n4l <oysterdays@gmail.com>
2023-06-07 23:48:20 +0800

    chore: refine install

--- chatsh.rb
+++ chatsh.rb
@@ -12,7 +12,7 @@ class Chatsh < Formula
         sha256 "b6f78ff57ec523d3d561e0487c843e79d14c76018d5c4152a6fc7bd12236247c"
         
         def install
-            bin.install "chatsh/chatsh"
+            bin.install "chatsh"
         end
     end
 
@@ -21,7 +21,7 @@ class Chatsh < Formula
         sha256 "072e8da51f64d39d36ba9a613a1b6911a7dce96abefc25fe0e0d6af195ccce7e"
         
         def install
-            bin.install "chatsh/chatsh"
+            bin.install "chatsh"
         end
     end
 end
