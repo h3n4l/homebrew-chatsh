613d6f1ab6bd1b2ac81679469d120c8805c79f71
h3n4l <oysterdays@gmail.com>
2023-06-07 23:51:28 +0800

    chore: update sha256 of release 0.0.1

--- chatsh.rb
+++ chatsh.rb
@@ -9,7 +9,7 @@ class Chatsh < Formula
 
     on_macos do
         url "https://github.com/h3n4l/chatsh/releases/download/0.0.1/chatsh-darwin_x86_64.tar.gz"
-        sha256 "b6f78ff57ec523d3d561e0487c843e79d14c76018d5c4152a6fc7bd12236247c"
+        sha256 "65b04615bf3e8131873f481fc85811f618a2085519d5aae99af5e1c0a7cd2542"
         
         def install
             bin.install "chatsh"
@@ -18,7 +18,7 @@ class Chatsh < Formula
 
     on_linux do
         url "https://github.com/h3n4l/chatsh/releases/download/0.0.1/chatsh-linux_x86_64.tar.gz"
-        sha256 "072e8da51f64d39d36ba9a613a1b6911a7dce96abefc25fe0e0d6af195ccce7e"
+        sha256 "67ce6eb78b3b74369c9ffbe08db88698f06a3bd8a3b084d69e842e2343bd5c1c"
         
         def install
             bin.install "chatsh"
