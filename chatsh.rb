4a1daa7c5da36fdf1eb684156209eed884f064ae
h3n4l <oysterdays@gmail.com>
2023-06-10 17:55:27 +0800

    revert: Revert "feat: update homebrew-h3n4l to 0.0.2" (#2)

--- chatsh.rb
+++ chatsh.rb
@@ -4,12 +4,12 @@
 class Chatsh < Formula
     desc "Translate text to commands in terminal."
     homepage "https://github.com/h3n4l/chatsh"
-    version "0.0.2"
+    version "0.0.1"
     license "MIT"
 
     on_macos do
-        url "https://github.com/h3n4l/chatsh/releases/download/0.0.2/chatsh-darwin_x86_64.tar.gz"
-        sha256 "7424d72714f8fdb65ade91d9c8ba8c433a4ee51d957edcb83fb7865009644deb"
+        url "https://github.com/h3n4l/chatsh/releases/download/0.0.1/chatsh-darwin_x86_64.tar.gz"
+        sha256 "65b04615bf3e8131873f481fc85811f618a2085519d5aae99af5e1c0a7cd2542"
         
         def install
             bin.install "chatsh"
@@ -17,8 +17,8 @@ class Chatsh < Formula
     end
 
     on_linux do
-        url "https://github.com/h3n4l/chatsh/releases/download/0.0.2/chatsh-linux_x86_64.tar.gz"
-        sha256 "7ced708edeb70ad8f75e6a21f35de796b2ec886720a3b74e5e6bc70b8368c11f"
+        url "https://github.com/h3n4l/chatsh/releases/download/0.0.1/chatsh-linux_x86_64.tar.gz"
+        sha256 "67ce6eb78b3b74369c9ffbe08db88698f06a3bd8a3b084d69e842e2343bd5c1c"
         
         def install
             bin.install "chatsh"
